32a109e76a758f3de411d91c118627082a5f814c
Jitendra Singh Bhadauria <jitendrasingh@Jitendras-MacBook-Air.local>
2026-07-02 17:25:50 +0530

    update axios file

--- src/Services/axios.js
+++ src/Services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://diginiwas-admin-backend.onrender.com/api",
 });
 
 export default  axiosInstance;
